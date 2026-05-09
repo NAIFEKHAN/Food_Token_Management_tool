700a3242b9406c21ea68a396317ffa6fea1e1a30
Naife Khan M <161137873+NAIFEKHAN@users.noreply.github.com>
2026-05-09 18:22:27 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from config import EVENT_NAME
 
 from .config import settings, EVENT_NAME
 from .database import Base, engine, SessionLocal
