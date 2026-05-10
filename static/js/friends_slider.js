e5fcf7427796a2b5bea1464f292cfaaf9685522c
Naife Khan M <161137873+NAIFEKHAN@users.noreply.github.com>
2026-05-10 12:54:07 +0530

    Add files via upload

---
+++ static/js/friends_slider.js
@@ -0,0 +1,39 @@
+const friends = [
+    { name: "PRAVEEN J", role: "Editor", image: "/static/images/friends/praveen.jpeg" },
+    { name: "ANNANGI SRINIVAS", role: "Volunteer", image: "/static/images/friends/srinivas.jpeg" },
+    { name: "JALLA BHARGAV", role: "Organizer", image: "/static/images/friends/bhargav.jpeg" },
+    { name: "KONDREDDY", role: "Designer", image: "/static/images/friends/koti.jpeg" },
+    { name: "ASHWIN A", role: "Designer", image: "/static/images/friends/ashwin.jpeg" },
+    { name: "PRAVEEN V M", role: "Designer", image: "/static/images/friends/cr.jpeg" },
+    { name: "DEVDHARSHAN J", role: "Designer", image: "/static/images/friends/dev.jpeg" },
+    { name: "GUGAN S", role: "Designer", image: "/static/images/friends/gugan.jpeg" },
+    { name: "KAVIPRIYAN M", role: "Designer", image: "/static/images/friends/kavi.jpeg" },
+    { name: "KUMARAGURUBARAN D", role: "Designer", image: "/static/images/friends/kumaraguru.jpeg" },
+    { name: "MUKESH R", role: "Designer", image: "/static/images/friends/mukesh.jpeg" },
+    { name: "NAIFE KHAN M", role: "Designer", image: "/static/images/friends/naife.jpeg" },
+    { name: "PRAKADESH", role: "Designer", image: "/static/images/friends/pragadeesh.jpeg" },
+    { name: "PRAVEEN P", role: "Designer", image: "/static/images/friends/praveenp.jpeg" },
+    { name: "SACHIN", role: "Designer", image: "/static/images/friends/sachin.jpeg" },
+    { name: "JAYASURIYA G", role: "Designer", image: "/static/images/friends/surya.jpeg" },
+    { name: "VIVEK REDDY", role: "Designer", image: "/static/images/friends/vivek.jpeg" },
+    { name: "JAYANTH REDDY", role: "Designer", image: "/static/images/friends/jayanth.jpeg" }
+];
+
+const track = document.getElementById('friends-track');
+
+if (track) {
+    const tripleList = [...friends, ...friends, ...friends];
+
+    tripleList.forEach(friend => {
+        const slide = document.createElement('div');
+        slide.className = 'friend-card';
+
+        slide.innerHTML = `
+        <img src="${friend.image}" alt="${friend.name}">
+        <span class="name">${friend.name}</span>
+        <span class="role">${friend.role}</span>
+`       ;
+
+        track.appendChild(slide);
+    });
+}
